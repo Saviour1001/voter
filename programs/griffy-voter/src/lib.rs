6a461cdcb31ae92d785356775a11c29c47369291
Saviour1001 <harshghodkar@gmail.com>
2023-11-11 17:19:12 +0530

    progress

--- programs/griffy-voter/src/lib.rs
+++ programs/griffy-voter/src/lib.rs
@@ -6,10 +6,135 @@ declare_id!("3qhJ4sBqUXCHT4AjDmgZRgb74ipymQuC159mq1KDRvzp");
 pub mod griffy_voter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializePolls>) -> Result<()> {
+        let poll_counter = &mut ctx.accounts.poll_counter;
+        poll_counter.admin = *ctx.accounts.admin.key;
+        poll_counter.total_polls = 0;
+        Ok(())
+    }
+
+    pub fn create_poll(ctx: Context<CreatePoll>, poll_question: String) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_question = poll_question;
+        poll.total_votes = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializePolls<'info> {
+    #[account(
+        init,
+        payer = admin,
+        space = PollCounter::size(),
+        seeds = ["poll_counter".as_bytes(), admin.key().as_ref()],
+        bump
+    )]
+    pub poll_counter: Account<'info, PollCounter>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreatePoll<'info> {
+    #[account(
+        init,
+        payer = admin,
+        space = Poll::size(),
+        seeds = ["poll_question".as_bytes()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const VECTOR_LENGTH: usize = 4;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_POLL_LENGTH: usize = 280 * 4; // 280 chars max.
+const MAX_VOTE_LENGTH: usize = 280 * 4; // 280 chars max.
+const U_64_LENGTH: usize = 8;
+
+#[account]
+#[derive(Default)]
+pub struct PollCounter {
+    pub admin: Pubkey, // admin's address
+    pub total_polls: u64, // total polls
+    pub bump: u8, // bump seed
+}
+
+#[account]
+pub struct Poll {
+    pub poll_question: String, // poll question
+    pub total_votes: u64, // total votes
+    pub bump: u8, // bump seed
+}
+
+#[account]
+pub struct Vote {
+    pub voter: Pubkey, // voter's address
+    pub vote: String, // timelock encrypted vote
+    pub amount: u64, // stake amount
+    pub bump: u8, // bump seed
+}
+
+impl PollCounter {
+    pub fn size() -> usize {
+        DISCRIMINATOR_LENGTH + // discriminator
+            PUBLIC_KEY_LENGTH + // admin
+            U_64_LENGTH + // total polls
+            1 // bump
+    }
+
+    pub fn new(admin: Pubkey, bump: u8) -> Self {
+        Self {
+            admin,
+            bump,
+            ..Default::default()
+        }
+    }
+
+    pub fn increment(&mut self) {
+        self.total_polls += 1;
+    }
+}
+
+impl Poll {
+    pub fn size() -> usize {
+        DISCRIMINATOR_LENGTH + // discriminator
+            (STRING_LENGTH_PREFIX + MAX_POLL_LENGTH) + // poll question
+            U_64_LENGTH + // total votes
+            1 // bump
+    }
+
+    pub fn new(poll_question: String, total_votes: u64, bump: u8) -> Self {
+        Self {
+            poll_question,
+            total_votes,
+            bump,
+        }
+    }
+}
+
+impl Vote {
+    pub fn size() -> usize {
+        DISCRIMINATOR_LENGTH + // discriminator
+            PUBLIC_KEY_LENGTH + // voter
+            (STRING_LENGTH_PREFIX + MAX_VOTE_LENGTH) + // vote
+            U_64_LENGTH + // amount
+            1 // bump
+    }
+
+    pub fn new(voter: Pubkey, vote: String, amount: u64, bump: u8) -> Self {
+        Self {
+            voter,
+            vote,
+            amount,
+            bump,
+        }
+    }
+}
--- tests/griffy-voter.ts
+++ tests/griffy-voter.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { GriffyVoter } from "../target/types/griffy_voter";
+import { expect } from "chai";
 
 describe("griffy-voter", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,65 @@ describe("griffy-voter", () => {
 
   const program = anchor.workspace.GriffyVoter as Program<GriffyVoter>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let adminKey: anchor.web3.Keypair;
+
+  before(async () => {
+    await getAdminKey();
   });
+
+  async function getAdminKey() {
+    adminKey = await generateFundedKeypair();
+  }
+
+  it("Is initialized!", async () => {
+    const [pollCounterPDA, _] =
+      await anchor.web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from(anchor.utils.bytes.utf8.encode("poll_counter")),
+          program.provider.publicKey.toBuffer(),
+        ],
+        program.programId
+      );
+
+    const tx = await program.methods.initialize().accounts({
+      admin: adminKey.publicKey,
+      pollCounter: pollCounterPDA,
+    });
+  });
+
+  it("Creates a poll!", async () => {
+    const [pollQuestionPDA, _] =
+      await anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from(anchor.utils.bytes.utf8.encode("poll_question"))],
+        program.programId
+      );
+
+    console.log("pollQuestionPDA", pollQuestionPDA.toBase58());
+
+    const tx = await program.methods
+      .createPoll("What is your favorite color?")
+      .accounts({
+        admin: adminKey.publicKey,
+        poll: pollQuestionPDA,
+      })
+      .signers([adminKey])
+      .rpc();
+  });
+
+  async function generateFundedKeypair(): Promise<anchor.web3.Keypair> {
+    const newKeypair = anchor.web3.Keypair.generate();
+
+    const transaction = new anchor.web3.Transaction().add(
+      anchor.web3.SystemProgram.transfer({
+        fromPubkey: program.provider.publicKey,
+        toPubkey: newKeypair.publicKey,
+        lamports: 5 * anchor.web3.LAMPORTS_PER_SOL,
+      })
+    );
+    const _confirmation = await (
+      program.provider as anchor.AnchorProvider
+    ).sendAndConfirm(transaction);
+
+    return newKeypair;
+  }
 });
